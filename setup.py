06ffa299548fb58bc2a1f95291dc63d166e869bc
Nikita Rodionov <nekonekt@gmail.com>
2017-09-11 17:34:26 +0300

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='knowdy-json-gateway',
-    version='0.0.5',
+    version='0.0.7',
     description='gateway to provide json-pure api',
     author='Globbie',
     classifiers=[
